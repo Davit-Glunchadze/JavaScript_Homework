6b68b1a938c4457a599b29a912bba97e4e1da3ed
David Glunchadze <david@CAC112131.mil.ge>
2025-03-10 17:13:30 +0400

    React_2_homework

--- main.js
+++ main.js
@@ -1,87 +1,73 @@
-// დავალება 1
-console.log("დავალება №1 შესრულებულია");
+//
+// Task 1------------------------------------
+//
+console.log("Task 1--------------------------------")
 
-function EqualTo(a, b) {
-  if (a === b) {
-    return "ტოლია";
-  } else {
-    return "არ არის ტოლი";
-  }
+const user = [
+    { name: "temo", age: 25 },
+    { name: "Lasha", age: 21 },
+    { name: "Ana", age: 28 },
+]
+
+function getYoungerAge(){
+    const getAges = user.map(person => person.age)
+    const youngest = Math.min(...getAges)
+    const youngestPerson = user.find(person => person.age === youngest)
+    console.log(youngestPerson)
 }
 
-//არ არის ტოლი
-console.log(EqualTo(1, 2));
-console.log(EqualTo("1", 1));
-console.log(EqualTo(true, 2));
-console.log(EqualTo("1", "2"));
+getYoungerAge ()
 
-//ტოლია
-console.log(EqualTo(2, 2));
-console.log(EqualTo("1", "1"));
-console.log(EqualTo(false, false));
+//
+// Task 2--------------------------------
+//
+console.log("Task 2--------------------------------")
 
-
-// დავალება 2
-console.log("დავალება №2 შესრულებულია");
-
-
-function fahrenheit(fahrenheit) {
-  if (typeof fahrenheit !== "number") {
-    return false;
-  } else {
-    return (fahrenheit - 32) / 1.8 + "℃";
-  }
+function cloneUser(original) {
+    return { ...original};
+    
 }
 
-// -17.22222222222222℃
-console.log(fahrenheit(1));
-
-//false
-console.log(fahrenheit(true));
-console.log(fahrenheit("1"));
-console.log(fahrenheit());
-
-// 0℃
-console.log(fahrenheit(32));
-
-
-
-// დავალება 3
-console.log("დავალება №3 შესრულებულია");
-
-
-function operation(a, operator, b) {
-  if (typeof a !== "number" || typeof b !== "number" ||typeof operator !== "string") {
-    return false;
-  }
-
-  if (operator === "/" && b === 0) {
-    return "ნულზე გაყოფა არ შეიძლება";
-  }
-  switch (operator) {
-    case "+":
-      return a + b;
-    case "-":
-      return a - b;
-    case "*":
-      return a * b;
-    case "/":
-      return a / b;
-    default:
-      return "მცდარი ოპერაცია";
-  }
+const original = {
+    name: 'David',
+    age: 35,
+    id: 3500 
 }
 
-//false
-console.log(operation(1, "+", "2"));
-console.log(operation("9", "*", 0));
-console.log(operation(9 + 0));
+const newUser = cloneUser(original) 
 
-//ნულზე გაყოფა არ შეიძლება
-console.log(operation(9, "/", 0));
+console.log(original)//name: 'David', age: 35, id: 3500
+console.log(newUser) //name: 'David', age: 35, id: 3500
 
-//მცდარი ოპერაცია
-console.log(operation(1, "=", 2));
+console.log(newUser === original) //false
 
-// 2025
-console.log(operation(2000, "+", 25));
+
+//
+// Task 3 ----------------------------
+//
+console.log("Task 3--------------------------------")
+
+    function dicePlayers(){
+    let a = parseInt(Math.random()*10)
+    console.log('Player A number is' + ' ' + a);
+
+    let b = parseInt(Math.random()*10)
+    console.log('Player B number is' + ' ' + b)
+    
+    switch (true) {
+        case (a === 3):
+            console.log('Player A is winner');
+            break;
+        case (b === 3):
+            console.log('Player B is winner');
+            break;
+        case (a === b):
+            console.log('It is a draw');
+            break;
+        default:
+            console.log('Try again');
+        }
+
+    }
+
+    dicePlayers()
