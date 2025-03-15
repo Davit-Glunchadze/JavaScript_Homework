d1649b39dec923de18452a77940b08b35cef2192
Davit-Glunchadze <davitiglunchadze@gmail.com>
2025-03-15 11:16:05 +0400

    Add my  homework

--- main.js
+++ main.js
@@ -1,73 +1,9 @@
-//
-// Task 1------------------------------------
-//
-console.log("Task 1--------------------------------")
+// Create an array
+let fruits = ["Apple", "Banana", "Cherry", "Date"];
 
-const user = [
-    { name: "temo", age: 25 },
-    { name: "Lasha", age: 21 },
-    { name: "Ana", age: 28 },
-]
+// Get the length of the array
+let lengthOfArray = fruits.length;
 
-function getYoungerAge(){
-    const getAges = user.map(person => person.age)
-    const youngest = Math.min(...getAges)
-    const youngestPerson = user.find(person => person.age === youngest)
-    console.log(youngestPerson)
-}
+// Output the length of the array
+console.log("The length of the array is: " + lengthOfArray);
 
-getYoungerAge ()
-
-//
-// Task 2--------------------------------
-//
-console.log("Task 2--------------------------------")
-
-function cloneUser(original) {
-    return { ...original};
-    
-}
-
-const original = {
-    name: 'David',
-    age: 35,
-    id: 3500 
-}
-
-const newUser = cloneUser(original) 
-
-console.log(original)//name: 'David', age: 35, id: 3500
-console.log(newUser) //name: 'David', age: 35, id: 3500
-
-console.log(newUser === original) //false
-
-
-//
-// Task 3 ----------------------------
-//
-console.log("Task 3--------------------------------")
-
-    function dicePlayers(){
-    let a = parseInt(Math.random()*10)
-    console.log('Player A number is' + ' ' + a);
-
-    let b = parseInt(Math.random()*10)
-    console.log('Player B number is' + ' ' + b)
-    
-    switch (true) {
-        case (a === 3):
-            console.log('Player A is winner');
-            break;
-        case (b === 3):
-            console.log('Player B is winner');
-            break;
-        case (a === b):
-            console.log('It is a draw');
-            break;
-        default:
-            console.log('Try again');
-        }
-
-    }
-
-    dicePlayers()
