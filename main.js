18b4b0f7f0993615c77d127bf017891ee290612e
David Glunchadze <davitiglunchadze@gmail.com>
2025-03-19 18:28:38 +0400

    Add homework 5 exercise

--- main.js
+++ main.js
@@ -1,9 +1,62 @@
-// Create an array
-let fruits = ["Apple", "Banana", "Cherry", "Date"];
+// task 1
 
-// Get the length of the array
-let lengthOfArray = fruits.length;
+const div1  = document.createElement('div')
+const btn = document.createElement('button')
+const testH1 = document.createElement('h1')
+testH1.textContent = 'Hello there'
+btn.textContent ='Text disappears'
 
-// Output the length of the array
-console.log("The length of the array is: " + lengthOfArray);
+  // classes
+div1.classList.add('div1')
+testH1.classList.add('testH1')
+btn.classList.add('btn')
+btn.setAttribute('id', 'btnid')
 
+  // append
+div1.append(btn, testH1)
+document.body.append(div1)
+div1.insertAdjacentHTML(
+  "afterbegin",
+  "Task 1"
+);
+
+const removerBtn = document.querySelector('#btnid')
+
+removerBtn.addEventListener('click', () => {
+  if (testH1.style.display === 'none') {
+    testH1.style.display = 'block';
+    btn.textContent ='Text disappears'
+  } else {
+    testH1.style.display = 'none';
+    btn.textContent = 'Text appears'
+  }
+})
+
+
+
+//task 2
+
+const div2 = document.createElement('div')
+const testH2 = document.createElement('h2')
+testH2.textContent = 'Gandalf'
+const aTag = document.createElement('a')
+aTag.textContent = 'Go to profile'
+aTag.href = '#'
+
+
+  //append
+div2.append(testH2, aTag)
+document.body.append(div2)
+
+div2.insertAdjacentHTML(
+  "afterbegin",
+  "Task 2"
+);
+
+  //classes, ID
+div2.setAttribute('id','card')
+
+/*  <div id=”card”>
+    <h2>Gandalf</h2>
+    <a href=”#”>Go to profile</a>
+    </div>                         */
